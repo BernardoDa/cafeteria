212e8791fb6dea8cdd114e734487f5477e8adfe2
BernardoDa <bernydacasa@gmail.com>
2024-08-08 01:10:56 -0600

    Terminado con source map

--- gulpfile.js
+++ gulpfile.js
@@ -1,25 +1,27 @@
-
-const { src, dest, watch, series, parallel } = require('gulp');
+const { src, dest, watch, series, parallel } = require("gulp");
 
 // CSS y SASS
-const sass = require('gulp-sass')(require('sass'));
-const postcss = require('gulp-postcss');
+const sass = require("gulp-sass")(require("sass"));
+const postcss = require("gulp-postcss");
 const autoprefixer = require("autoprefixer");
+const sourcemaps = require("gulp-sourcemaps");
 
 // Imagenes
-const imagemin = require('gulp-imagemin');
-const webp = require('gulp-webp');
-const avif = require('gulp-avif');
+const imagemin = require("gulp-imagemin");
+const webp = require("gulp-webp");
+const avif = require("gulp-avif");
 
-function css( done ){
-    //compilar sass
-    // pasos: 1 - Identificar archivo, 2 - Compilarla, 3 - Guardar el .css
+function css(done) {
+  //compilar sass
+  // pasos: 1 - Identificar archivo, 2 - Compilarla, 3 - Guardar el .css
 
-    src('src/scss/app.scss')
-        .pipe( sass( ) )
-        .pipe( postcss( [autoprefixer()]))
-        .pipe( dest('build/css'))
-    done();
+  src("src/scss/app.scss")
+    .pipe(sourcemaps.init())
+    .pipe(sass())
+    .pipe(postcss([autoprefixer()]))
+    .pipe(sourcemaps.write("."))
+    .pipe(dest("build/css"));
+  done();
 }
 
 function imagenes() {
@@ -28,31 +30,28 @@ function imagenes() {
     .pipe(dest("build/img"));
 }
 
-function versionWebp(){
-    const opciones = {
-        quality: 50
-    }
-    return src('src/img/**/*.{png,jpg}')
-        .pipe( webp(opciones))
-        .pipe( dest('build/img'))
+function versionWebp() {
+  const opciones = {
+    quality: 50,
+  };
+  return src("src/img/**/*.{png,jpg}")
+    .pipe(webp(opciones))
+    .pipe(dest("build/img"));
 }
 
 function versionAvif() {
-    const opciones = {
-        quality: 50
-    }
-    return src('src/img/**/*.{png,jpg}')
-        .pipe( avif(opciones))
-        .pipe( dest('build/img'))
+  const opciones = {
+    quality: 50,
+  };
+  return src("src/img/**/*.{png,jpg}")
+    .pipe(avif(opciones))
+    .pipe(dest("build/img"));
 }
-
-
 
 function dev() {
-    watch("src/scss/**/*.scss", css);
-    watch("src/img/**/*", imagenes);
+  watch("src/scss/**/*.scss", css);
+  watch("src/img/**/*", imagenes);
 }
-
 
 exports.css = css;
 exports.dev = dev;
